9920ce43328c0253b933bf6485aaf490f536871a
Daniyal <danidina330@gmail.com>
2024-01-15 09:22:23 +0100

    add camera example

---
+++ examples/cam_movement.rs
@@ -0,0 +1,90 @@
+use app::*;
+
+fn main()
+{
+    baguette::new()
+        .add_loop::<TestA>()
+        .run()
+}
+
+struct TestA
+{
+    timer: u8,
+    cam: &'static mut Camera,
+    sprite: Sprite,
+}
+
+impl State for TestA
+{
+    fn new(app: &mut App) -> Self where Self: Sized
+    {
+        Self
+        {
+            timer: 0,
+            cam: Camera::main_mut(),
+            sprite: app.renderer.load_sprite
+            (
+                SpriteLoader::SpriteSheet
+                {
+                    path: r"assets\green dude sheet.png",
+                    filtermode: FilterMode::Nearest,
+                    instances: vec!
+                    [
+                        (
+                            Transform
+                            {
+                                translation: (0.,0.,1.).into(),
+                                ..Default::default()
+                            },
+                            SheetTiles::RangeIn(19..=21)
+                        )
+                    ],
+                    pxunit: 100.,
+                    layout: SpriteLayout { rows: 6, columns: 5 }
+                }
+            )
+        }
+    }
+
+    fn update(&mut self, app: &mut App<'_>, _: &StateEvent)
+    {
+        self.move_cam(&app.input);
+
+        match self.timer == 0
+        {
+            true =>
+            {
+                for instance in self.sprite.iter_instances_mut()
+                {
+                    instance.section.next_or_first();
+                }
+
+                self.timer = 8
+            }
+            false => self.timer -= 1
+        }
+    }
+}
+
+impl TestA
+{
+    fn move_cam(&mut self, input: &input::Input)
+    {
+        if input.get_key_holding(input::KeyCode::KeyW)
+        {
+            self.cam.set_position(self.cam.position() + (math::Vec3::Z * -0.1))
+        }
+        if input.get_key_holding(input::KeyCode::KeyS)
+        {
+            self.cam.set_position(self.cam.position() + (math::Vec3::Z * 0.1))
+        }
+        if input.get_key_holding(input::KeyCode::KeyA)
+        {
+            self.cam.set_position(self.cam.position() + (math::Vec3::X * -0.1))
+        }
+        if input.get_key_holding(input::KeyCode::KeyD)
+        {
+            self.cam.set_position(self.cam.position() + (math::Vec3::X * 0.1))
+        }
+    }
+}
