0f607826d2a55b1d94f7f8cd8a4cf574f59b46e9
Daniyal <danidina330@gmail.com>
2024-05-19 18:27:28 +0200

    update cam movement example

--- examples/cam_movement.rs
+++ examples/cam_movement.rs
@@ -11,59 +11,42 @@ struct TestA
 {
     timer: u8,
     cam: Camera,
-    sprite: Sprite,
+    sprite: SpriteSheet,
 }
 
 impl State for TestA
 {
     fn new(app: &mut App) -> Self where Self: Sized
     {
-        todo!()
-        //Self
-        //{
-        //    timer: 0,
-        //    cam: Camera::get(&mut app.renderer),
-        //    sprite: app.renderer.load_sprite
-        //    (
-        //        SpriteLoader::SpriteSheet
-        //        {
-        //            path: r"assets\green dude sheet.png",
-        //            filtermode: FilterMode::Nearest,
-        //            instances: vec!
-        //            [
-        //                (
-        //                    Transform
-        //                    {
-        //                        translation: (0.,0.,1.).into(),
-        //                        ..Default::default()
-        //                    },
-        //                    SheetTiles::RangeIn(19..=21)
-        //                )
-        //            ],
-        //            pxunit: 100.,
-        //            layout: SpriteLayout { rows: 6, columns: 5 }
-        //        }
-        //    )
-        //}
+        Self
+        {
+            timer: 0,
+            cam: Camera::get(&mut app.renderer),
+            sprite: SpriteSheet::new
+            (
+                &mut app.renderer,
+                SpriteSheetLoader::new_pixelated(r"assets\green dude sheet.png", [], 6, 5)
+            )
+        }
     }
 
     fn update(&mut self, app: &mut App, _: &StateEvent)
     {
         self.move_cam(&app.input);
-        todo!()
-        //match self.timer == 0
-        //{
-        //    true =>
-        //    {
-        //        for instance in self.sprite.iter_mut()
-        //        {
-        //            instance.section.next_or_first();
-        //        }
 
-        //        self.timer = 8
-        //    }
-        //    false => self.timer -= 1
-        //}
+        match self.timer == 0
+        {
+            true =>
+            {
+                for (.., section) in self.sprite.iter_mut()
+                {
+                    section.next_or_first();
+                }
+
+                self.timer = 8
+            }
+            false => self.timer -= 1
+        }
     }
 }
 
