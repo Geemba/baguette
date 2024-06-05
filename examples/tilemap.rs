3d8a387f3f52e9267db24cf889dec60c4a91a6ee
Daniyal <danidina330@gmail.com>
2024-06-05 20:08:57 +0200

    tilemap example add layers

--- examples/tilemap.rs
+++ examples/tilemap.rs
@@ -11,12 +11,20 @@ struct AppWithTileMap;
 
 impl State for AppWithTileMap
 {
-    fn new(app: &mut App) -> Self where Self: Sized
+    fn new(app: &mut App) -> Self
     {
         app.renderer.add_tilemap
         (
             TilemapBuilder::default()
+                .add_layer::<0>
+                (
+                    [
+                        Tile{ pos: (0.0 ,0.1).into(), ..Default::default() },
+                        Tile{ pos: (0.9 ,0.1).into(), ..Default::default() }
+                    ]
+                )
                 .add_texture("assets/green dude.png", 1, 1)
+                
         );
 
         Self
