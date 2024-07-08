8eb21db3149c346a9e5184159a061657d2b5f406
Daniyal <danidina330@gmail.com>
2024-07-08 19:36:50 +0200

    refactor

--- crates/baguette-app/src/dispatch/dynamic.rs
+++ crates/baguette-app/src/dispatch/dynamic.rs
@@ -1,84 +1,62 @@
-//! dynamically dispatched
 use crate::*;
 
-/// [Transition] is composed of a `predicate` function and the [StateId]
-/// it should transition to if the predicate returns `true`
-pub type Transition = (fn(&mut App, &DispatchedState) -> bool, StateId);
-
-/// how the state type is dispatched 
-type DispatchedState = Box<dyn State>;
-/// a function pointer that returns the state concrete type
-type StateReturnCallback = fn(&mut App) -> DispatchedState;
-/// a function pointer that returns a vec containing all the possible transitions
-type TransitionsCallback = fn() -> Vec<Transition>;
-
 /// a state of this application
-pub trait State
+pub trait AppState
 {
     /// describe how to initialize the implementor 
     fn new(app: &mut App) -> Self where Self: Sized;
 
     fn update(&mut self, app: &mut App, event: &StateEvent);
 
-    fn id() -> StateId
-    where
-        Self: core::any::Any + Sized
+    fn transitions(&self, _: &App) -> Option<StateId>
     {
-        StateId(core::any::TypeId::of::<Self>())
+        None
+    }
+
+    fn id() -> StateId where Self: Sized + 'static
+    {
+        StateId::of::<Self>()
     }
 }
 
-struct Dummy;
-
 /// an empty state to use as default
-impl State for Dummy
+impl AppState for Dummy
 {
-    fn new(_ : &mut App) -> Self where Self : Sized
+    fn new(_ : &mut App) -> Self
     {
         Dummy
     }
 
-    fn update(&mut self, _:&mut App, _ : &StateEvent) {}
-}
-
-impl State for ()
-{
-    fn new(_ : &mut App) where Self : Sized {}
-    fn update(&mut self, _:&mut App, _ : &StateEvent) {}
+    fn update(&mut self, _: &mut App, _: &StateEvent) {}
 }
 
 pub struct ActiveState
 {
     id: StateId,
-    state: DispatchedState,
+    state: Box<dyn AppState>,
+    getter: fn(&mut App) -> Box<dyn AppState>,
+    /// dictates which event will be called in the update function
     event: StateEvent,
-    transitions: Vec<Transition>,
-    to_unactive: (StateReturnCallback, TransitionsCallback)
 }
+
 impl ActiveState
 {
     fn into_unactive(self) -> UnactiveState
     {
-        UnactiveState
-        {
-            id: self.id,
-            state: self.to_unactive.0,
-            transitions: self.to_unactive.1,
-        }
+        UnactiveState { activator: self.getter, id: self.id }
     }
 
-    fn avaiable_transition(&mut self, app: &mut App) -> Option<&Transition>
+    fn avaiable_transition(&mut self, app: &mut App) -> Option<StateId>
     {
-        self.transitions.iter().find(|transition| (transition.0)(app, &self.state))
+        self.state.transitions(app)
     }
 }
 
 /// unactive states holds data to be able to reactivate themselves
 pub struct UnactiveState
 {
-    id: StateId,
-    state: fn(&mut App) -> DispatchedState,
-    transitions: fn() -> Vec<Transition>
+    activator: fn(&mut App) -> Box<dyn AppState>,
+    id: StateId
 }
 
 impl UnactiveState
@@ -88,11 +66,10 @@ impl UnactiveState
     {
         ActiveState
         {
-            id: self.id,
-            state: (self.state)(application),
+            state: (self.activator)(application),
             event: StateEvent::default(),
-            transitions: (self.transitions)(),
-            to_unactive: (self.state, self.transitions),
+            getter: self.activator,
+            id: self.id,
         }
     }
 }
@@ -100,9 +77,9 @@ impl UnactiveState
 pub struct FsmData<T>
 {
     // this will be the first state that enters the statemachine
-    current : T,
+    current: T,
     // vec of unactive states so that we dont instantiate anything that calls on uninitialized application data 
-    states : ahash::AHashMap<StateId, UnactiveState>
+    states: ahash::AHashMap<StateId, UnactiveState>
 }
 
 impl Default for FsmData<UnactiveState>
@@ -111,12 +88,7 @@ impl Default for FsmData<UnactiveState>
     {
         Self
         {
-            current : UnactiveState
-            {
-                id : <Dummy as State>::id(),
-                transitions: Vec::new,
-                state: |_| Box::new(())
-            },
+            current: UnactiveState { activator: |_| Box::new(Dummy), id: StateId::default()},
             states: ahash::AHashMap::new()
         }
     }
@@ -129,26 +101,23 @@ impl FsmData<UnactiveState>
     /// # panics
     /// 
     /// panics if the state was already added 
-    pub fn add_state<St>(&mut self, transitions: fn() -> Vec<Transition>)
+    pub fn add_state<T>(&mut self)
     where
-        St: State + 'static
+        T: AppState + 'static
     {
-        assert!
-        (
-            !self.states.contains_key(&<St>::id()) && self.current.id != <St>::id(),
-            "attempted to add the same state twice 👮"
-        );
+        let id = StateId::of::<T>();
 
         let state = UnactiveState
         {
-            id: <St>::id(),
-            transitions,
-            state: |app| Box::new(<St>::new(app))
+            id, activator: |app| Box::new(T::new(app))
         };
 
-        match self.current.id != <Dummy as State>::id()
+        match self.current.id != StateId::default()
         {
-            true => { self.states.insert(<St>::id(), state); }
+            true =>
+            {
+                self.states.insert(id, state);
+            }
             false => self.current = state
         }
     }
@@ -157,7 +126,7 @@ impl FsmData<UnactiveState>
     {
         FsmData
         {
-            current: self.current.into_active(app), states : self.states
+            current: self.current.into_active(app), states: self.states
         }
     }
 
@@ -176,20 +145,25 @@ impl FsmData<ActiveState>
 
         match &self.current.event
         {  
-            StateEvent::Update => if let Some((.., state_id)) = self.current.avaiable_transition(app)
+            StateEvent::Update => if let Some(state_id) = self.current.avaiable_transition(app)
             {
-                self.current.event = StateEvent::Exit(*state_id)
+                self.current.event = StateEvent::Exit(state_id)
             }
             StateEvent::Enter => self.current.event = StateEvent::Update,
 
             StateEvent::Exit(id) =>
             {
                 let new = self.states.remove(id)
-                .expect("attempted to transition to a state that wasn't found, probably because it wasn't added to the fsm 👮")
+                .expect
+                (
+                    "attempted to transition to a state that wasn't found,
+                    probably because it wasn't added to the fsm"
+                )
                 .into_active(app);
 
-                let old = std::mem::replace(&mut self.current, new).into_unactive();
-                self.states.insert(old.id, old);
+                let old = std::mem::replace(&mut self.current, new);
+
+                self.states.insert(old.id, old.into_unactive());
             }
         }
     }
